13a61cba6bb8e3379a91f5cb56fe19127bbceb7f
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-11-13 13:34:06 -0300

    quinto commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ExerciciosJs
+## ExerciciosJs
 Resolução dos exercícios dos módulos de Java Script do curso de Web moderno da Cod3r
+
+## Aprendizado
+
+Varios exercícios foram resolvidos, varias lagrimas tambem foi derramada kkkkk, mais com a dedicação certa vc é capaz de chegar lá...
+
+## Assunto
+
+-- Vetor
+-- Funçoes
+-- Condicionais de controle
+-- ArrowFunction
+-- Fundamentos do Js em geral
--- exercicio15.js
+++ exercicio15.js
@@ -2,10 +2,12 @@ const escolherCarro = modelo =>{
     switch (modelo){
         case 'hatch':
             console.log('Compra efetuada com sucesso')
+            break;
         case 'sedan':
         case 'motocicleta':
         case 'caminhote':
             console.log('Tem certeza que nao prefere este modelo?')
+            break;
         default:
             return 'Não trabalhamos com este tipo de automovel aqui'
     }
